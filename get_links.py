fea37e04769b7fc675c330b6c2932488e4698afb
moses okoth <jackmokn@gmail.com>
2017-01-10 16:50:58 +0300

    Update get_links.py

--- get_links.py
+++ get_links.py
@@ -1,21 +1,37 @@
 # This program will extract all the links from a webpage
-import sys
-import urllib2
 import re
-url="ubuntuforums.org"
-sys.stdout.write("Wait a Minute its retrieving.")
-try:
-    if url[0:4]!="http":
-        url="http://" + url
-        f=(urllib2.urlopen(url)).read()
-        k=re.findall('(href)="(\S+)"',f)
-        k=set(k)
-        print "The Links are:------------"
-        for x in k:
-            if len(x[1])>2:
-                print x[1] +"\n"
-except:
-    print "NOT found plz check url"
-
-
-        
+import sys
+import urllib
+import urlparse
+from BeautifulSoup import BeautifulSoup
+ 
+class MyOpener(urllib.FancyURLopener):
+    version = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.15) Gecko/20110303 Firefox/3.6.15'
+ 
+def process(url):
+    myopener = MyOpener()
+    #page = urllib.urlopen(url)
+    page = myopener.open(url)
+ 
+    text = page.read()
+    page.close()
+ 
+    soup = BeautifulSoup(text)
+ 
+    for tag in soup.findAll('a', href=True):
+        tag['href'] = urlparse.urljoin(url, tag['href'])
+        print tag['href']
+# process(url)
+ 
+def main():
+    if len(sys.argv) == 1:
+        print "Jabba's Link Extractor v0.1"
+        print "Usage: %s URL [URL]..." % sys.argv[0]
+        sys.exit(-1)
+    # else, if at least one parameter was passed
+    for url in sys.argv[1:]:
+        process(url)
+# main()
+ 
+if __name__ == "__main__":
+    main()
